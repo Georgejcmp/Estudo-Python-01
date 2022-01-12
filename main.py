595d3c2a901b333da2fa5797173a176c6f381de6
Georgejcmp <george.moreno@yahoo.com.br>
2022-01-12 18:35:13 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,31 @@
+# Variáveis - Estudo de Python
+
+x = 10
+y = 20
+z = 100
+w = (x + y) * z/100
+print(w)
+
+print('Esse texto será impresso no console')
+print(x)
+print('Texto e duas variáveis', 'x','z')
+
+m = 'python'
+print(type(m))
+print(type(x))
+
+print('Informe o valor')
+i = input()
+print(type(i))
+
+var = input('Informe o valor')
+var = int(var)
+print(type(var))
+var = float(var)
+print(type(var))
+
+# Este código é um comentário
+
+x = 10 # Este código também é um comentário
+''' Aqui pode ser outra 
+forma de cometário '''
